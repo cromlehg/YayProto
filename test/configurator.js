9f2f649c5a96a8e0f143ba40ade6cba2866fa972
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-02-05 22:16:27 +0300

    chore(test): fix test for Configurator

--- test/configurator.js
+++ test/configurator.js
@@ -11,19 +11,31 @@ const should = require('chai')
   .should();
 
 const Configurator = artifacts.require('Configurator.sol');
+const Token = artifacts.require('YayProtoToken.sol');
+const Presale = artifacts.require('Presale.sol');
+const Mainsale = artifacts.require('Mainsale.sol');
 
-contract('Configurator', function (accounts) {
+contract('Configurator integration test', function (accounts) {
   let configurator;
+  let token;
+  let presale;
+  let mainsale;
 
   before(async function () {
     // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
     await advanceBlock();
     configurator = await Configurator.new();
-    configurator.depoy();
+    await configurator.deploy();
+    const tokenAddress = await configurator.token();
+    const presaleAddress = await configurator.presale();
+    const mainsaleAddress = await configurator.mainsale();
+    token = await Token.at(tokenAddress);
+    presale = await Presale.at(presaleAddress);
+    mainsale = await Mainsale.at(mainsaleAddress);
   });
 
-  it('should have adresses for token and crowdsales', async function () {
-    const token = await configurator.token();
-    await console.log(token);
+  it('contracts should have token address', async function () {
+    const tokenOwner = await token.owner();
+    await console.log(tokenOwner);
   });
 });
